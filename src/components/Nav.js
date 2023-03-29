2dc25a6ca5b70c6081b1ddc4527457f56c32d780
Dinma-P <phaytep@gmail.com>
2023-03-29 16:34:50 +0100

    Add responsive link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll";
 
 const Nav = () => {
-    const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
   const links = [
     {
@@ -44,27 +44,36 @@ const Nav = () => {
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-#fff hover:scale-105 duration-200"
           >
-                        <Link to={link} smooth duration={500}>{link}</Link>
-
+            <Link
+             to={link} smooth duration={500}>
+              {link}
+            </Link>
           </li>
         ))}
       </ul>
 
-      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-white md:hidden" >
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-white md:hidden"
+      >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gray-400 text-#fff">
-      {links.map(({ id, link }) => (
-          <li
-            key={id}
-            className="px-4 cursor-pointer capitalize py-6 text-4xl"
-          >
-            {link}
-          </li>
-        ))}
-      </ul>
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gray-400 text-#fff">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link
+              onClick={() => setNav(!nav)}
+               to={link} smooth duration={500}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
